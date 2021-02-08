b918184ff6672065987b200f74030f7c8b0b75e7
Beerschtein <anbatalov@inbox.ru>
2021-02-08 18:07:58 +0300

    Initial commit

---
+++ Практическое задание 5/task_3.py
@@ -0,0 +1,9 @@
+with open('f_file_3.txt', 'r', encoding='utf-8') as f:
+    workers = {}
+    for line in f:
+        key, value = line.split()
+        workers[key] = value
+        if int(value) < 20000:
+            print(f'{key}: зарплата меньше 20000')
+            print(f'Средняя зарплата {int(value/(sum(key)))}')
+
